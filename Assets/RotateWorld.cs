596d6475e5cd5c98ac80b1eb7811031c178a52ef
Danny Wilson <danny@dannywilson.ca>
2013-10-08 19:36:53 -0300

    Initial Commit

---
+++ Assets/RotateWorld.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateWorld : MonoBehaviour
+{
+	public float wait = 5;
+	public float rotationAngle = 90;
+	public float speed = 5;
+	
+	public GameObject world;
+	
+	//state variables
+	private bool rotatable = true;
+	private float lastRotate;
+	private bool rotating;
+
+	// Use this for initialization
+	void Start ()
+	{
+		lastRotate = Time.time - wait;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		rotatable = lastRotate + wait <= Time.time;
+		if(rotating){
+			Quaternion newRotation = Quaternion.AngleAxis(rotationAngle, new Vector3(0, 0, 1));
+			world.transform.rotation = Quaternion.Slerp(world.transform.rotation, newRotation, speed * Time.deltaTime);
+			
+			if(transform.rotation == newRotation){
+				rotating = false;
+			}
+		}
+		if(!rotatable){
+			this.light.intensity = 0;
+		} else {
+			this.light.intensity = 7;
+		}
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player" && rotatable && !rotating) {
+			rotating = true;
+			lastRotate = Time.time;
+		}
+	
+	}
+}
---
+++ Assets/SmoothFollow2D.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class SmoothFollow2D : MonoBehaviour {
+ 
+    public float dampTime = 0.15f;
+	public Transform target;
+    private Vector3 velocity = Vector3.zero;
+
+	private Vector3 delta;
+	
+	void Start(){
+
+		if (target)
+       	{
+			delta = transform.position - target.position ;
+		}
+		
+	}
+    // Update is called once per frame
+    void Update () 
+    {
+       if (target)
+       {
+			Vector3 destination = target.position + delta;
+        	transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+       }
+ 
+    }
+}
