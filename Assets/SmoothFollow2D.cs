e722f75f4177513eef7073f16aff211564882e7a
Danny Wilson <dannywils@gmail.com>
2013-10-22 11:21:36 -0300

    Add space skybox and starting level zoom

--- Assets/SmoothFollow2D.cs
+++ Assets/SmoothFollow2D.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class SmoothFollow2D : MonoBehaviour {
  
     public float dampTime = 0.15f;
+	public float zoomSpeed = 2;
+	public float finalZoom = 9;
 	public Transform target;
+	
     private Vector3 velocity = Vector3.zero;
-
 	private Vector3 delta;
+	private float zoom;
 	
 	void Start(){
 
@@ -24,6 +27,7 @@ public class SmoothFollow2D : MonoBehaviour {
        {
 			Vector3 destination = target.position + delta;
         	transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+			camera.orthographicSize =  Mathf.Lerp(camera.orthographicSize, finalZoom, zoomSpeed * Time.deltaTime);
        }
  
     }
