398d5255d77cd1e50e0821fdce68979e48a1673a
Danny Wilson <danny@dannywilson.ca>
2013-11-07 13:22:33 -0400

    Slowly zoom with FOV

--- Assets/SmoothFollow2D.cs
+++ Assets/SmoothFollow2D.cs
@@ -5,12 +5,13 @@ public class SmoothFollow2D : MonoBehaviour {
  
     public float dampTime = 0.15f;
 	public float zoomSpeed = 2;
-	public float finalZoom = 9;
+	public float startFOV = 180;
 	public Transform target;
 	
     private Vector3 velocity = Vector3.zero;
 	private Vector3 delta;
 	private float zoom;
+	private float endFOV = 30;
 	
 	void Start(){
 
@@ -18,6 +19,8 @@ public class SmoothFollow2D : MonoBehaviour {
        	{
 			delta = transform.position - target.position ;
 		}
+		camera.fieldOfView = startFOV;
+		
 		
 	}
     // Update is called once per frame
@@ -27,7 +30,7 @@ public class SmoothFollow2D : MonoBehaviour {
        {
 			Vector3 destination = target.position + delta;
         	transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-			camera.orthographicSize =  Mathf.Lerp(camera.orthographicSize, finalZoom, zoomSpeed * Time.deltaTime);
+			camera.fieldOfView =  Mathf.Lerp(camera.fieldOfView, endFOV, zoomSpeed * Time.deltaTime);
        }
  
     }
