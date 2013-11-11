3280e5c39673939f042a11e772bf87d53144d171
Danny Wilson <dannywils@gmail.com>
2013-11-11 13:05:59 -0400

    Merge menus/ui into master

---
+++ Assets/GUI Scripts/LevelSelection.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelection : MonoBehaviour {
+
+	public GUISkin menuSkin;
+	string menu = "";
+	void OnGUI()
+	{
+		GUI.skin = menuSkin;
+		
+		var style = new GUIStyle();
+		style.fontSize = 50;
+		style.normal.textColor = Color.white;
+		
+		GUI.Label(new Rect(Screen.width/2 - 200, Screen.height/2 - 200, 0, 0), "Dekeract", style);
+		GUI.BeginGroup(new Rect(Screen.width/2 - 150, Screen.height/2 - 100, 300, 300));
+		
+		GUI.Box(new Rect(0, 0, 300, 300),"Choose a Level");
+		
+		if (GUI.Button(new Rect (20,50,50,50), "1"))
+			Application.LoadLevel("Level1");
+		
+		if (GUI.Button(new Rect (90,50,50,50), "2"))
+			Application.LoadLevel("Level2");
+		
+		if (GUI.Button(new Rect (160,50,50,50), "3"))
+			Application.LoadLevel("Level3");
+		
+		if (GUI.Button(new Rect (20,120,50,50), "4"))
+			Application.LoadLevel("Level4");
+		
+		if (GUI.Button(new Rect (90,120,50,50), "5"))
+			Application.LoadLevel("Level5");
+		
+		if (GUI.Button(new Rect (160,120,50,50), "6"))
+			Application.LoadLevel("Level6");
+		
+		if (GUI.Button(new Rect (20,190,50,50), "7"))
+			Application.LoadLevel("Level7");
+		
+		if (GUI.Button(new Rect (90,190,50,50), "8"))
+			Application.LoadLevel("Level8");
+		
+		if (GUI.Button(new Rect (160,190,50,50), "9"))
+			Application.LoadLevel("Level9");
+		
+		if (GUI.Button(new Rect (230,190,50,50), "10"))
+			Application.LoadLevel("Level10");
+			
+		if (GUI.Button(new Rect (135,265,160,30), "Back"))
+			Application.LoadLevel("MainMenu");
+		GUI.EndGroup();
+	}
+}
---
+++ Assets/GUI Scripts/MainMenu.cs
@@ -0,0 +1,60 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+	public GUISkin menuSkin;
+	string menu = "";
+	void OnGUI()
+	{
+		GUI.skin = menuSkin;
+		
+		var style = new GUIStyle();
+		style.fontSize = 50;
+		style.normal.textColor = Color.white;
+		
+		GUI.Label(new Rect(Screen.width/2 - 200, Screen.height/2 - 200, 0, 0), "Dekeract", style);
+		
+		switch(menu)
+		{
+			case "help":
+				GUI.BeginGroup(new Rect(Screen.width/2 - 250, Screen.height/2 - 100, 500, 500));
+				GUI.Box(new Rect(0, 0, 500, 300),"");
+				GUI.Label(new Rect(10, 10, 490, 290), "ADD HELP DATA");
+				if (GUI.Button(new Rect (335,265,160,30), "Back"))
+					menu = "";
+				break;
+			case "storyline":
+				GUI.BeginGroup(new Rect(Screen.width/2 - 250, Screen.height/2 - 100, 500, 500));
+				GUI.Box(new Rect(0, 0, 500, 300),"");
+				GUI.Label(new Rect(10, 10, 490, 290), "ADD STORYLINE DATA");
+				if (GUI.Button(new Rect (335,265,160,30), "Back"))
+					menu = "";
+				break;
+			case "about":
+				GUI.BeginGroup(new Rect(Screen.width/2 - 250, Screen.height/2 - 100, 500, 500));
+				GUI.Box(new Rect(0, 0, 500, 300),"");
+				GUI.Label(new Rect(10, 10, 490, 290), "This game was created for a final project in our game design class " +
+					"(CSCI 4168) at Dalhousie University. The game is created by:");
+				GUI.Label(new Rect(20, 100, 490, 20), "Danny Wilson");
+				GUI.Label(new Rect(20, 130, 490, 20), "Rob Moore");
+				GUI.Label(new Rect(20, 160, 490, 20), "Matthew Coelho");
+				if (GUI.Button(new Rect (335,265,160,30), "Back"))
+					menu = "";
+				break;
+			default:
+				GUI.BeginGroup(new Rect(Screen.width/2 - 100, Screen.height/2 - 100, 200, 200));
+				GUI.Box(new Rect(0, 0, 300, 300),"");
+				if (GUI.Button(new Rect (20,30,160,30), "Play"))
+					Application.LoadLevel("LevelSelection");
+				if (GUI.Button(new Rect (20,70,160,30), "Help"))
+					menu = "help";
+				if (GUI.Button(new Rect (20,110,160,30), "Storyline"))
+					menu = "storyline";
+				if (GUI.Button(new Rect (20,150,160,30), "About"))
+					menu = "about";
+				break;
+		}
+		GUI.EndGroup();
+	}
+}
