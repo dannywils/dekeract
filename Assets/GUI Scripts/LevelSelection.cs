97e00a0ab803f75008bdc7a27ef5b481d00b1422
Danny Wilson <dannywils@gmail.com>
2013-11-11 19:51:37 -0400

    Fix rotation bug

--- Assets/GUI Scripts/LevelSelection.cs
+++ Assets/GUI Scripts/LevelSelection.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class LevelSelection : MonoBehaviour {
 
 	public GUISkin menuSkin;
-	string menu = "";
+
 	void OnGUI()
 	{
 		GUI.skin = menuSkin;
--- Assets/Scripts/RotateWorld.cs
+++ Assets/Scripts/RotateWorld.cs
@@ -14,31 +14,30 @@ public class RotateWorld : MonoBehaviour
 	private bool rotatable = true;
 	private float lastRotate;
 	private bool rotating;
+	private Quaternion newRotation;
 
 	// Use this for initialization
 	void Start ()
 	{
 		lastRotate = Time.time - wait;
+		newRotation = Quaternion.AngleAxis(rotationAngle, new Vector3(0, 0, 1));
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		rotatable = lastRotate + wait <= Time.time;
+		
 		if(rotating){
-			Quaternion newRotation = Quaternion.AngleAxis(rotationAngle, new Vector3(0, 0, 1));
-			
-			world.transform.rotation = Quaternion.Slerp(world.transform.rotation, newRotation, speed * Time.deltaTime);
-
-			Compass game = compass.GetComponent<Compass>();
-			game.angle = -world.transform.rotation.eulerAngles.z;
-
-			if(transform.rotation == newRotation){
+			if(Mathf.Abs(world.transform.rotation.eulerAngles.z - newRotation.eulerAngles.z) <= 0.1f){
 				rotating = false;
-				transform.rotation = newRotation;
-			}
-			
+				world.transform.rotation = newRotation;
+				return;
+			}			
+			world.transform.rotation = Quaternion.Slerp(world.transform.rotation, newRotation, speed * Time.deltaTime);
+			compass.GetComponent<Compass>().angle = -world.transform.rotation.eulerAngles.z;
 		}
+		
 		if(!rotatable){
 			this.light.intensity = 0;
 		} else {
