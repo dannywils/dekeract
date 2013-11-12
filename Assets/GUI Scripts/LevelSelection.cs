b9cd6757fe98b1496c306b047002550f5b73b692
Danny Wilson <dannywils@gmail.com>
2013-11-12 15:39:48 -0400

    Dynamic level select and audio manager

--- Assets/GUI Scripts/LevelSelection.cs
+++ Assets/GUI Scripts/LevelSelection.cs
@@ -18,38 +18,21 @@ public class LevelSelection : MonoBehaviour {
 		
 		GUI.Box(new Rect(0, 0, 300, 300),"Choose a Level");
 		
-		if (GUI.Button(new Rect (20,50,50,50), "1"))
-			Application.LoadLevel("Level1");
+		int count = 0;
+		for(int i = 0; i < 2; i++){
+			for(int j = 0; j < 5; j++){
+				count++;
+				if (GUI.Button(new Rect (15 + 55 * j , 95 * i + 45,50,90), count.ToString())){
+					Application.LoadLevel("Level" + count);
+				}
+			}
+		}
 		
-		if (GUI.Button(new Rect (90,50,50,50), "2"))
-			Application.LoadLevel("Level2");
-		
-		if (GUI.Button(new Rect (160,50,50,50), "3"))
-			Application.LoadLevel("Level3");
-		
-		if (GUI.Button(new Rect (20,120,50,50), "4"))
-			Application.LoadLevel("Level4");
-		
-		if (GUI.Button(new Rect (90,120,50,50), "5"))
-			Application.LoadLevel("Level5");
-		
-		if (GUI.Button(new Rect (160,120,50,50), "6"))
-			Application.LoadLevel("Level6");
-		
-		if (GUI.Button(new Rect (20,190,50,50), "7"))
-			Application.LoadLevel("Level7");
-		
-		if (GUI.Button(new Rect (90,190,50,50), "8"))
-			Application.LoadLevel("Level8");
-		
-		if (GUI.Button(new Rect (160,190,50,50), "9"))
-			Application.LoadLevel("Level9");
-		
-		if (GUI.Button(new Rect (230,190,50,50), "10"))
-			Application.LoadLevel("Level10");
-			
-		if (GUI.Button(new Rect (135,265,160,30), "Back"))
+		if (GUI.Button(new Rect (135,265,160,30), "Back")){
 			Application.LoadLevel("MainMenu");
+		}
+
+		
 		GUI.EndGroup();
 	}
 }
--- Assets/Scripts/MenuSound.cs
+++ Assets/Scripts/MenuSound.cs
@@ -1,18 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class MenuSound : MonoBehaviour {
+	
+	public AudioClip MenuMusic;
+	public AudioClip[] LevelMusic;
+	
 	void Awake ()
 	{
 		DontDestroyOnLoad (gameObject);
 	}
 	// Use this for initialization
 	void Start () {
-	
+		audio.PlayOneShot(MenuMusic);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+	
+	void OnLevelWasLoaded(int level) {
+		if(LevelMusic.Length > level-2 && level >= 2){
+			audio.Stop();
+			audio.PlayOneShot(LevelMusic[level-2]);
+		}
+    }
+	
 }
