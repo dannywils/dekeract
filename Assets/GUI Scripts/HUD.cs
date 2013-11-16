40defc80ec17577cc668b3a60741424da1a6c90d
Matthew Coelho <mcoelho@northrock.bm>
2013-11-15 21:02:42 -0400

    Added pause functionality and pause menu to the HUD

--- Assets/GUI Scripts/HUD.cs
+++ Assets/GUI Scripts/HUD.cs
@@ -6,15 +6,39 @@ public class HUD : MonoBehaviour {
 	public GUISkin HUDSkin;
 	public Texture PauseButton, Heart;
 	public int PlayerLives;
+	bool paused = false;
 	
 	void OnGUI ()
 	{
 		GUI.skin = HUDSkin;
 		
-		if(PauseButton == null)
-			GUI.Button(new Rect(Screen.width-60, 10, 50, 50), "||");
-		else
-			GUI.Button(new Rect(Screen.width-60, 10, 50, 50), PauseButton);
+		if(GUI.Button(new Rect(Screen.width-60, 10, 50, 50), "||"))
+			paused = true;
+		
+		if(paused)
+		{
+			Time.timeScale = 0.0f;
+			GUI.BeginGroup (new Rect (Screen.width / 2 - 150, Screen.height / 2 - 100, 300, 250));
+			GUI.Box (new Rect (0, 0, 300, 200), "Game Paused");
+			if (GUI.Button (new Rect (25, 50, 250, 30), "Resume"))
+			{
+				paused = false;
+				Time.timeScale = 1.0f;
+			}
+			if (GUI.Button (new Rect (25, 100, 250, 30), "Restart Level"))
+			{
+				paused = false;
+				Time.timeScale = 1.0f;
+				Application.LoadLevel(Application.loadedLevel);
+			}
+			if (GUI.Button (new Rect (25, 150, 250, 30), "Main Menu"))
+			{
+				paused = false;
+				Time.timeScale = 1.0f;
+				Application.LoadLevel("MainMenu");
+			}
+			GUI.EndGroup();
+		}
 		
 		GUI.Label(new Rect(10, Screen.height-40, 75, 30), "Lives:");
 		for(int i = 0; i < PlayerLives; i++)
