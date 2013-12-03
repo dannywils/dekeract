1277d95017629284d5674a06134cb7ccc77985a1
Matthew Coelho <mcoelho@northrock.bm>
2013-12-03 19:05:23 -0400

    Fixed the flame jets

--- Assets/Scripts/ParticleKillPlayer.cs
+++ Assets/Scripts/ParticleKillPlayer.cs
@@ -3,22 +3,12 @@ using System.Collections;
 
 public class ParticleKillPlayer : MonoBehaviour
 {
-	private ParticleSystem.CollisionEvent[] collisionEvents = new ParticleSystem.CollisionEvent[16];
-
-	void OnParticleCollision(GameObject other)
-	{
-		int safeLength = particleSystem.safeCollisionEventSize;
-
-		if (collisionEvents.Length < safeLength)
-			collisionEvents = new ParticleSystem.CollisionEvent[safeLength];
-		
-		int numCollisionEvents = particleSystem.GetCollisionEvents(other, collisionEvents);
-		int i = 0;
-		while (i < numCollisionEvents) {
-			if (other.gameObject.tag == "Player") {
-				Application.LoadLevel(Application.loadedLevelName);
+	void OnTriggerEnter (Collider other) {
+		if(particleSystem.particleCount > 2)
+		{
+			if(other.tag == "Player"){
+				Application.LoadLevel(Application.loadedLevel);
 			}
-			i++;
 		}
 	}
 }
